b19e5cfcd7771140b792c4e0b1ef09685db7de58
LD101 <jmuendongei@gmail.com>
2023-05-25 18:11:41 +0300

    Invoke

--- Assets/Scripts/Collision_Handler.cs
+++ Assets/Scripts/Collision_Handler.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Collision_Handler : MonoBehaviour
 {
+    [SerializeField] private float waitTime;
       
      
     private void OnCollisionEnter(Collision other)
@@ -15,17 +16,42 @@ public class Collision_Handler : MonoBehaviour
                 Debug.Log("friend");
                 break;
             case "Finished":
-                Debug.Log("finito");
+                NextScene();
                 break;
             case "Fuel":
                 Debug.Log("fueled");
                 break;
             case "Obstacle":
-                Debug.Log("obstacle");
+                CrashSequence();
                 break;
         }
         
     }
 
-    
+    private void CrashSequence()
+    {
+        ReloadLevel();
+        Invoke("ReloadLevel", + waitTime); //invoke
+        GetComponent<Movement>().enabled = false;
+    }
+
+    private void NextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentScene + 1;
+        SceneManager.LoadScene(1);
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextScene", + waitTime); //invoke
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex; 
+        SceneManager.LoadScene(currentScene);
+    }
 }
